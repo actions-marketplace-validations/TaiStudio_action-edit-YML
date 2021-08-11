3b59657400281bf2b95481ef9a6eebbb6f7c618a
Tai Studio <tai.studio@outlook.fr>
2021-08-11 16:01:47 +0200

    🖋 Update

--- main.js
+++ main.js
@@ -17,27 +17,42 @@ const fs = require('fs');
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const type = core.getInput('type');
+    console.log(`Hello ${nameToGreet}!`, typeof nameToGreet);
+
+    switch(type){
+        case 'single':
+            single();
+            break;
+        case 'multiple':
+            break;
+        default:
+            single();
+
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
 
-// try {
-//     const doc = yaml.load(fs.readFileSync('./dist/latest.yml', 'utf8'));
-//     doc['files'][0]['url'] = replaceALL(doc['files'][0]['url'], '-', '.');
-//     doc['path'] = replaceALL(doc['path'], '-', '.');
-//     console.log(doc);
+function single(){
+    console.log('bite');
+}
 
-//     var newFile = yaml.dump(doc);
-//     fs.writeFileSync('test.yml', newFile, 'utf8');
-// } catch (e) {
-//     console.log(e);
-// }
+function editor(file, strPARAM, find, replace){
+    try {
+        const doc = yaml.load(fs.readFileSync('./dist/latest.yml', 'utf8'));
+        // const doc = yaml.load(fs.readFileSync(file, 'utf8'));
+        doc['files'][0]['url'] = replaceALL(doc['files'][0]['url'], '-', '.');
+        // doc['files'][0]['url'] = replaceALL(doc['files'][0]['url'], find, replace);
+        doc['path'] = replaceALL(doc['path'], '-', '.');
+        console.log(doc);
+    
+        var newFile = yaml.dump(doc);
+        fs.writeFileSync('test.yml', newFile, 'utf8');
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 function replaceALL(str, find, replace) {
     var escapedFind=find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
