323aee5097e59c955e0fc5c3f8bca6e0044ac222
Tai Studio <tai.studio@outlook.fr>
2021-08-20 12:21:47 +0200

    🖋 Update

--- main.js
+++ main.js
@@ -42,13 +42,9 @@ function single(files, replace, replaceFor){
 function editor(file, strPARAM, find, replace){
     try {
         const doc = yaml.load(fs.readFileSync(file, 'utf8'));
-        doc['files'][0]['url'] = replaceALL(doc['files'][0]['url'], '-', '.');
-        // doc['files'][0]['url'] = replaceALL(doc['files'][0]['url'], find, replace);
-        doc['path'] = replaceALL(doc['path'], '-', '.');
-        console.log(doc);
-    
+        doc['files'][0]['url'] = replaceALL(strPARAM, find, replace);
         var newFile = yaml.dump(doc);
-        fs.writeFileSync('test.yml', newFile, 'utf8');
+        fs.writeFileSync(file, newFile, 'utf8');
     } catch (e) {
         console.log(e);
     }
